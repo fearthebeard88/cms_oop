315fd779dd83edfb5aef942d6c074b96ddb2fa20
Tim <tferris88@gmail.com>
2018-06-10 15:45:12 -0400

    i hate dumb mistakes that i already know how to fix

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -4,16 +4,21 @@ class User {
 
 public static function find_all_users() {
     global $db;
-    $result_set = $db -> query("SELECT * FROM users");
-    return $result_set;
+    return self :: find_query("SELECT * FROM users ");
 }
 
 public static function find_user($id) {
     global $db;
-$results = $db -> query("SELECT * FROM users WHERE id = {$id} LIMIT 1");
+$results = self :: find_query("SELECT * FROM users WHERE id = {$id} LIMIT 1 ");
 $found_user = mysqli_fetch_array($results);
 
 return $found_user;
+}
+
+public static function find_query($sql) {
+    global $db;
+    $result_set = $db -> query($sql);
+    return $result_set;
 }
 
 }
