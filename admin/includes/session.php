370f583091355d24ad9fada56646709ac6eb4e40
Tim <tferris88@gmail.com>
2018-06-11 21:11:25 -0400

    session start, login, logout

---
+++ admin/includes/session.php
@@ -0,0 +1,44 @@
+<?php
+
+class Session {
+
+    private $signed_in = false;
+    public $user_id;
+
+    function __construct() {
+        session_start();
+        $this -> check_login();
+    }
+
+    private function check_login() {
+        if(isSet($_SESSION['user_id'])) {
+            $this -> user_id = $_SESSION['user_id'];
+            $this -> signed_in = true;
+        } else {
+            unset($this -> user_id);
+            $this -> signed_in = false;
+        }
+    }
+
+    public function is_signed_in() {
+        return $this -> $signed_in;
+    }
+
+    public function login($user) {
+        if($user) {
+            $this -> user_id = $_SESSION['user_id'] = $user -> id;
+            $this -> signed_in = true;
+        }
+    }
+
+    public function logout($user) {
+        unset($_SESSION['user_id']);
+        unset($this -> user_id);
+        $this -> signed_in = false;
+    }
+
+}
+
+$session = new Session();
+
+?>
