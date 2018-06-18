dd677a168b58491e69b0330d94a9ebcf772f2c41
Tim <tferris88@gmail.com>
2018-06-17 20:16:23 -0400

    login done

--- admin/login.php
+++ admin/login.php
@@ -16,30 +16,35 @@ if(isSet($_POST['submit'])) {
         $session -> login($user_found);
         redirect('index.php');
     } else {
+        $the_message = "You shall not pass.";
+    }
+ } 
+ 
+ else {
         $username = '';
         $password = '';
-        echo "You shall not pass.";
+        $the_message = '';
     }
-}
+
 
 ?>
 
 
 <div class="col-md-4 col-md-offset-3">
 
-<h4 class="bg-danger"><?php // echo $the_message; ?></h4>
+<h4 class="bg-danger"><?php echo $the_message; ?></h4>
 	
 <form id="login-id" action="" method="post">
 	
 <div class="form-group">
 	<label for="username">Username</label>
-	<input type="text" class="form-control" name="username" value="<?php // echo htmlentities($username); ?>" >
+	<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
 
 </div>
 
 <div class="form-group">
 	<label for="password">Password</label>
-	<input type="password" class="form-control" name="password" value="<?php // echo htmlentities($password); ?>">
+	<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
 	
 </div>
 
