e835dd79f85a3439b95bed7c39deedd74d755003
Tim <tferris88@gmail.com>
2018-06-27 20:48:43 -0400

    user picture

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -4,12 +4,19 @@
 class User extends DB_Object {
     // properties available to this class
     protected static $db_table = "users";
-    protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name');
+    protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name', 'user_image');
     public $id;
     public $username;
     public $first_name;
     public $last_name;
     public $password;
+    public $user_image;
+    public $upload_directory = "images";
+    public $image_placeholder = "http://via.placeholder.com/200x200&text=image";
+
+    public function user_picture() {
+        return empty($this -> user_image) ? $this -> image_placeholder : "includes" . DS . $this -> upload_directory . DS . $this -> user_image;
+    }
 
 public static function verify_user($username, $password) {
     global $db;
