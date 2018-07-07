b22146013dd313c0fcec661b4c43ce634b45225c
Tim <tferris88@gmail.com>
2018-07-06 21:07:49 -0400

    still figuring it out

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -2,19 +2,21 @@
 
 class Comment extends DB_Object {
     protected static $db_table = 'comments';
-    protected static $db_table_fields = array('id', 'photo_id', 'author', 'body');
+    protected static $db_table_fields = array('id', 'photo_id', 'author', 'body', 'comment_count');
     public $id;
     public $photo_id;
     public $author;
     public $body;
+    public $comment_count;
 
-    public static function create_comment($photo_id, $author, $body) {
+    public static function create_comment($photo_id, $author, $body, $comment_count) {
         if(!empty($photo_id) && !empty($author) && !empty($body)) {
             $comment = new Comment();
 
             $comment->photo_id = (int)$photo_id;
             $comment->author = $author;
             $comment->body = (string)$body;
+            $comment->comment_count = (int)$comment_count + 1;
 
             return $comment;
         } else {
