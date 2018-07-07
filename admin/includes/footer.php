02f0546262b2345e0c2c4f11dcca270556fdf5c5
Tim <tferris88@gmail.com>
2018-07-07 12:08:48 -0400

    i like this chart

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -16,15 +16,18 @@
 
         var data = google.visualization.arrayToDataTable([
           ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
+          ['Views', <?php echo $session->count; ?>],
+          ['Photos', <?php echo Photo::count_all(); ?>],
+          ['Users', <?php echo User::count_all(); ?>],
+          ['Comments', <?php echo Comment::count_all(); ?>]
         ]);
 
         var options = {
-          title: 'My Daily Activities'
+          title: 'Views, Photos, Users, and Comments',
+          is3D: 'true',
+          slices: {
+            0: {offset: 0.5}
+          }
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
