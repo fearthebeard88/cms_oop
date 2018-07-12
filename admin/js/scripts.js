9bd69ec503b004b0d851aaee3e384654b7ad29e3
Tim <tferris88@gmail.com>
2018-07-11 20:50:20 -0400

    more friggin javascript

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
+    var user_id;
     var image_src;
     var image_split;
-    var image_id;
+    var image_name;
 
     $(".modal_thumbnails").click(function() {
         $("#set_user_image").prop('disabled', false);
-        var user_id = $.urlParam('id');
+        user_id = $.urlParam('id');
         $.ajax({
             url: 'edit_user.php',
             data: user_id,
@@ -14,11 +15,26 @@ $(document).ready(function() {
         });
         image_src = $(this).prop("src");
     image_split = image_src.split("/");
-    image_id = image_split[image_split.length-1];
-    alert(image_id);
+    image_name = image_split[image_split.length-1];
     
     tinymce.init({ selector:'textarea' });
     })
+
+    $("#set_user_image").click(function() {
+        $.ajax({
+            url: "includes/ajax_code.php",
+            data: {
+                image_name: image_name,
+                user_id: user_id
+            },
+            type: "POST",
+            success: function(data) {
+                if(!data.error) {
+                    alert(image_name);
+                }
+            }
+        })
+    });
     
 })
 
